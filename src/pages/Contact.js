3c5ac35a6be260bacb5db61bcc8e1514c3883c1c
Viktoria Siigur <viktoria.siigur@gmail.com>
2022-03-20 18:28:24 +0100

    Improve style

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,44 +1,58 @@
-import Card from "../components/Card";
+import Card from '../components/Card';
 
-import classes from "./Contact.module.scss";
-import emailIcon from "../assets/icons/email.png";
-import phoneIcon from "../assets/icons/phone.png";
-import facebookIcon from "../assets/icons/facebook.png";
+import classes from './Contact.module.scss';
+
+import emailIcon from '../assets/icons/email.png';
+import phoneIcon from '../assets/icons/phone.png';
+import facebookIcon from '../assets/icons/facebook.png';
+import viktor from '../assets/viktor.jpeg';
 
 const Contact = () => {
   return (
     <div className={classes.content}>
       <Card>
         <h2>Võta meiega ühendust:</h2>
-        <div className={classes.contactLine}>
+        <br />
+        <div className={classes.header}>
           <img
-            className={classes.icon}
-            src={emailIcon}
-            alt="Emaili ikoon"
+            className={classes.picture}
+            src={viktor}
+            alt="Viktor Siigur"
           ></img>
-          viktor.siigur@gmail.com
+          <br />
+          <div>Viktor Siigur</div>
         </div>
-        <div className={classes.contactLine}>
-          <img
-            className={classes.icon}
-            src={phoneIcon}
-            alt="Telefoni ikoon"
-          ></img>
-          +372 56631515
-        </div>
-        <div className={classes.contactLine}>
-          <img
-            className={classes.icon}
-            src={facebookIcon}
-            alt="Telefoni ikoon"
-          ></img>
-          <a
-            href="https://www.facebook.com/ViMaTransport"
-            target="_blank"
-            rel="noreferrer"
-          >
-            ViMaTransport
-          </a>
+        <div className={classes.contactLines}>
+          <div className={classes.contactLine}>
+            <img
+              className={classes.icon}
+              src={emailIcon}
+              alt="Emaili ikoon"
+            ></img>
+            viktor.siigur@gmail.com
+          </div>
+          <div className={classes.contactLine}>
+            <img
+              className={classes.icon}
+              src={phoneIcon}
+              alt="Telefoni ikoon"
+            ></img>
+            +372 56631515
+          </div>
+          <div className={classes.contactLine}>
+            <img
+              className={classes.icon}
+              src={facebookIcon}
+              alt="Telefoni ikoon"
+            ></img>
+            <a
+              href="https://www.facebook.com/ViMaTransport"
+              target="_blank"
+              rel="noreferrer"
+            >
+              ViMaTransport
+            </a>
+          </div>
         </div>
 
         {/* <tr>
